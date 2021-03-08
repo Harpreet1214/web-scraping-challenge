e90de990d99f2db349ff1d27afce4f919b36947a
Singh <gagans@amazon.com>
2021-03-08 11:48:49 -0800

    updating the jupyter notebook

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -6,35 +6,54 @@ import time
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 
+executable_path = {"executable_path": ChromeDriverManager().install()}
+
+
 def scrape():
-    #browser = Browser("chrome", **executable_path, headless=False)
+    browser = Browser("chrome", **executable_path, headless=False)
 
     mars_data = {}
-
-#Set the executable path and initialize the chrome browser in splinter
-executable_path = {"executable_path": ChromeDriverManager().install()}
-browser = Browser("chrome", **executable_path, headless=False)
 
 # NASA Mars News
 # Visitng the Mars NASA News website
 url = "https://mars.nasa.gov/news/"
-browser.visit(url)
-html = browser.html
+response = requests.get(url)
 
 # Create BeautifulSoup object with html.parser
-soup = bs(html, 'html.parser')
+soup = bs(response.text, 'html.parser')
 
 # Search for news titles
-news_titles = soup.find_all('div', class_='content_title')
+results = soup.find_all('div', class_='content_title')
 
-#Searching paragraph text under news title
-p_results = soup.find_all('div', class_='article_teaser_body')
+#Creating a blank list to hold the headlines
+news_titles = []
 
-#Extracting first title and paragraph
-news_firsttitle = news_titles[0].text
-news_firstp = p_results[0].text
+#Creating loop over div elements
+for result in results:
+    if(result.a):
+        if(result.a.text):
+            news_titles.append(result)
+news_titles
 
-#adding titles and paragraph to dictionary
+finalnewstitles = []
+
+#Printing the headlines only
+for x in range(6):
+    var = news_titles[x].text
+    newvar = var.strip('\n\n')
+    finalnewstitles.append(newvar)
+
+#Find classification for description paragraph below title
+presults = soup.find_all('div', class_="rollover_description_inner")
+
+news_p = []
+# Loop through the div results to pull out just the text
+for x in range(6):
+        var=presults[x].text
+        newvar = var.strip('\n\n')
+        news_p.append(newvar)
+    
+#add titles and paragraphs to dictionary
 mars_data['news_titles'] = finalnewstitles
 mars_data['news_p'] = news_p
 
@@ -54,7 +73,8 @@ soup_img = bs(img_browser, "html.parser")
 
 a = soup_img.find("div", class_="BaseImagePlaceholder")
 
-a.find("img")["data-src"]
+featured_img_url = a.find("img")["data-src"]
+featured_img_url
 
 #Mars facts
 # URL to be scraped
@@ -64,57 +84,84 @@ url = "https://space-facts.com/mars/"
 table = pd.read_html(url)
 
 # Asking for facts
-mars_facts = table[0]
-mars_facts
+mars_df = table[0]
+mars_df
 
-mars_facts.to_html()
+mars_df.columns = ['Data', 'Measurement']
+mars_df.head()
+
+m = pd.Series(mars_df['Data'])
+mars_df['Data'] = m.str.strip(':')
+mars_df = mars_df.set_index('Data')
+mars_df
+
+# Converting into html
+html_table = mars_df.to_html()
+html_table
+
+#Saving as html file
+mars_df.to_html('mars_table.html')
 
 #Mars Hemispheres
-img_url = []
-title = []
-hemisphere_image_urls = []
-
 url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-html = requests.get(url).text
+browser.visit(url)
+nextpage_urls = []
+imgtitles = []
+base_url = 'https://astrogeology.usgs.gov'
+
+#Html object
+html = browser.html
+
+#Using Beautiful Soup
 soup = bs(html, 'html.parser')
 
-results_main_page=soup.find_all("div", class_="item")
+#Getting the info for eleents containing hemisphere photo data
+divs = soup.find_all('div', class_='description')
 
-for x in range(len(results_main_page)):
-    image_title = results_main_page[x].a.h3
-    image_title = image_title.get_text()
-    title.append(image_title)
+#Iteration to pull titles
+counter = 0
+for div in divs:
+    link = div.find('a')
+    href = link['href']
+    img_title = div.a.find('h3')
+    img_title = img_title.text
+    imgtitles.append(img_title)
+    next_page = base_url + href
+    nextpage_urls.append(next_page)
+    counter = counter+1
+    if (counter == 4):
+        break
+print(nextpage_urls)
+print(imgtitles)
 
-    link = results_main_page[x].a["href"]
-    main_site = "https://astrogeology.usgs.gov"
-    image_link = main_site + link
-    url = image_link
-    
-    html = requests.get(url).text
+#Creating loop for high resolution photo on next page
+my_images = []
+for nextpage_url in nextpage_urls:
+    url = nextpage_url
+    browser.visit(url)
+    html = browser.html
     soup = bs(html, 'html.parser')
-    
-    results=soup.select("ul>li")
-    
-    for x in range(len(results)):
-        if "original" in (results[x].get_text()).lower():
-            each_img_url = results[x].a["href"]
-            img_url.append(each_img_url)
-            
-            img_urls_dic={
-                "title":image_title,
-                "img_url":each_img_url
-            }
-            
-            hemisphere_image_urls.append(img_urls_dic)
-            
-            break
+    link2 = soup.find('img', class_="wide-image")
+    forfinal = link2['src']
+    full_img = base_url + forfinal
+    my_images.append(full_img)
+    nextpage_urls = []
+my_images
 
-hemisphere_image_urls
+hemispheres_image_urls = []
+cerberus = {'title': imgtitles[0], 'img_url': my_images[0]}
+schiaparelli = {'title': imgtitles[1], 'img_url': my_images[1]}
+syrtis = {'title': imgtitles[2], 'img_url': my_images[2]}
+valles = {'title': imgtitles[3], 'img_url': my_images[3]}
+
+hemispheres_image_urls = [cerberus, schiaparelli, syrtis, valles]
+
+print(hemispheres_image_urls)
 
 #adding to dict
-mars_data["hemisphere_image_urls"] = hemisphere_image_urls
+    mars_data["hemisphere_image_urls"] = hemisphere_image_urls
 
-return mars_data
+    return mars_data
 
 if __name__ == "__main__":
     scrape()
